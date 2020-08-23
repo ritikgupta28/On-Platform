dcda4321a80245336c427f80a1c0be4f3d6bd43b
ritikgupta28 <54578783+ritikgupta28@users.noreply.github.com>
2020-08-23 16:50:24 +0530

    Update AllContestsCard.js

--- src/components/admin_page/all_contests/AllContestsCard.js
+++ src/components/admin_page/all_contests/AllContestsCard.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 
 export default class AllContestsCard extends React.Component {
 	render() {
 		return (
-			<div className='quescard'>
+			<Container style={{ fontSize: '20px', textAlign: 'center', border: '1px solid black'}}>
+			<div className='questioncard'>
 				<Link to={`/allcontests/questions/${this.props.id}`}>
 					<p>{this.props.id}</p>
 				</Link>
 				<Link to={`/result/${this.props.id}`}>
-					<button className='but' value={this.props.id}>{this.props.sign}</button> 
+					<button value={this.props.id}>{this.props.sign}</button> 
 				</Link>
 			</div>
+			</Container>
 		)
 	}
 }
