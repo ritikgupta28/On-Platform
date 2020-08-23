353e008f65294d5542c05ddbdce80a3cdf3483c6
sahilgoyals1999 <sahilgoyals1999@users.noreply.github.com>
2020-08-24 01:02:16 +0530

    adding modal and backdrop

--- src/components/error_handler/ErrorHandler.js
+++ src/components/error_handler/ErrorHandler.js
@@ -1,22 +1,30 @@
-import React, { Fragment } from 'react';
-
-import Backdrop from '../bbackdrop/Backdrop';
-import Modal from '../modall/Modal';
+import React from 'react';
+import { Modal, Button } from 'react-bootstrap'
 
 const errorHandler = props => (
-  <Fragment>
-  {props.error && <Backdrop onClick={props.onHandle} />}
+  <div>
     {props.error && (
       <Modal
-        title="An Error Occurred"
-        onCancelModal={props.onHandle}
-        onAcceptModal={props.onHandle}
-        acceptEnabled
+        show="true"
+        onHide={props.onHandle}
+        backdrop="static"
+        keyboard={false}
       >
+      <Modal.Dialog>
+       <Modal.Header closeButton>
+        <Modal.Title>An Error Occurred</Modal.Title>
+       </Modal.Header>
+       <Modal.Body>
         <p>{props.error.message}</p>
-      </Modal>
+       </Modal.Body>
+       <Modal.Footer>
+        <Button variant="secondary" onClick={props.onHandle}>Close</Button>
+        <Button variant="primary" onClick={props.onHandle}>Accept</Button>
+       </Modal.Footer>
+       </Modal.Dialog>
+       </Modal>
     )}
-  </Fragment>
+  </div>
 );
 
 export default errorHandler;
