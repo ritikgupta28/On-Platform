274ef1411fd993f4059c3849b21f237af54be3fe
ritikgupta28 <54578783+ritikgupta28@users.noreply.github.com>
2020-08-23 16:56:02 +0530

    Update QuestionContestCard.js

--- src/components/admin_page/QuestionContestCard.js
+++ src/components/admin_page/QuestionContestCard.js
@@ -1,15 +1,19 @@
 import React from 'react'
-import './admin/Admin.css'
+import { Container } from 'react-bootstrap'
 import { Link } from 'react-router-dom';
+
+import './admin/Admin.css'
 
 export default class Card extends React.Component {
 	render() {
 		return (
-			<div className='quescard'>
-				<Link to={`/question/${this.props.id}`}>
-					<p>{this.props.title}</p>
-				</Link>
-			</div>
+			<Container style={{ fontSize: '20px', textAlign: 'center', border: '1px solid black'}}>
+				<div className='questioncard'>
+					<Link to={`/question/${this.props.id}`}>
+						<p>{this.props.title}</p>
+					</Link>
+				</div>
+			</Container>
 		)
 	}
 }
