8bf00c61fa8e814f96fc3845445255d644cddd1e
ritikgupta28 <54578783+ritikgupta28@users.noreply.github.com>
2020-08-23 16:59:58 +0530

    Update FinalContestCard.js

--- src/components/user_page/final_contest/FinalContestCard.js
+++ src/components/user_page/final_contest/FinalContestCard.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 
 export default class FinalContestCard extends React.Component {
 	render() {
 		return (
-			<div className='quescard'>
+			<Container style={{ fontSize: '20px', textAlign: 'center', border: '1px solid black'}}>
+			<div className='questioncard'>
 				<Link to={`/finalcontest/questions/${this.props.id}`}>
 					<p>{this.props.id}</p>
 				</Link>
 			</div>
+			</Container>
 		)
 	}
 }
