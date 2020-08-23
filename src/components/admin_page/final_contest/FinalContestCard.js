009f21838108ad6818507b97452211735221c7aa
ritikgupta28 <54578783+ritikgupta28@users.noreply.github.com>
2020-08-23 16:53:17 +0530

    Update FinalContestCard.js

--- src/components/admin_page/final_contest/FinalContestCard.js
+++ src/components/admin_page/final_contest/FinalContestCard.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+
+import '../admin/Admin.css';
 
 export default class CardContest extends React.Component {
 	render() {
 		return (
-			<div className='quescard'>
+			<Container style={{ fontSize: '20px', textAlign: 'center', border: '1px solid black'}}>
+			<div className='questioncard'>
 				<Link to={`/finalcontest/questions/${this.props.id}`}>
 					<p>{this.props.id}</p>
 				</Link>
 				<Link to="/admin/allcontests">
-					<button className='but' value={this.props.id} onClick={this.props.handle}>{this.props.sign}</button> 
+					<button value={this.props.id} onClick={this.props.handle}>{this.props.sign}</button> 
 				</Link>
 			</div>
+			</Container>
 		)
 	}
 }
