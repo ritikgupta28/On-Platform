47175ffba0d027dc5b5dc930a67fb75eda2a342c
ritikgupta28 <54578783+ritikgupta28@users.noreply.github.com>
2020-08-23 17:00:56 +0530

    Update FinalContestQuestions.js

--- src/components/user_page/final_contest/FinalContestQuestions.js
+++ src/components/user_page/final_contest/FinalContestQuestions.js
@@ -1,59 +1,59 @@
 import React, { Component } from 'react';
 import Card from './QuestionContestCard';
-import ErrorHandler from '../../ErrorHandler/ErrorHandler'
+import ErrorHandler from '../../error_handler/ErrorHandler'
 
 class FinalContestQuestions extends Component {
-  state = {
-    questions: [],
-    error: null
-  };
+	state = {
+		questions: [],
+		error: null
+	};
 
-  catchError = error => {
-    this.setState({ error: error })
-  }
+	catchError = error => {
+		this.setState({ error: error })
+	}
 
-  errorHandler = () => {
-    this.setState({ error: null });
-  };
+	errorHandler = () => {
+		this.setState({ error: null });
+	};
 
-  componentDidMount() {
-    const contestId = this.props.match.params.id;
-    fetch('http://localhost:8000/feed/userfinalcontest/questions/' + contestId, {
-      headers: {
-        Authorization: 'Bearer ' + this.props.token,
-        'Content-Type': 'application/json'
-      }
-      })
-      .then(res => {
-        if (res.status !== 200) {
-          throw new Error('error');
-        }
-        return res.json();
-      })
-      .then(resData => {
-        this.setState({
-          questions: resData.questions
-        });
-      })
-      .catch(this.catchError);
-  }
+	componentDidMount() {
+		const contestId = this.props.match.params.id;
+		fetch('http://localhost:8000/feed/userfinalcontest/questions/' + contestId, {
+			headers: {
+				Authorization: 'Bearer ' + this.props.token,
+				'Content-Type': 'application/json'
+			}
+		})
+		.then(res => {
+			if(res.status !== 200) {
+				throw new Error('error');
+			}
+			return res.json();
+		})
+		.then(resData => {
+			this.setState({
+				questions: resData.questions
+			});
+		})
+		.catch(this.catchError);
+	}
 
-  render() {
-    return (
-      <div>
-        <ErrorHandler error={this.state.error} onHandle={this.errorHandler} />
-        {
-          this.state.questions.map(q=> (
-            <Card
-            key={q.questionId._id}
-            id={q.questionId._id}
-            title={q.questionId.title}
-          />
-          ))
-        }
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div>
+				<ErrorHandler error={this.state.error} onHandle={this.errorHandler} />
+				{
+					this.state.questions.map(q=> (
+						<Card
+							key={q.questionId._id}
+							id={q.questionId._id}
+							title={q.questionId.title}
+						/>
+					))
+				}
+			</div>
+		)
+	}
 }
 
 export default FinalContestQuestions;
