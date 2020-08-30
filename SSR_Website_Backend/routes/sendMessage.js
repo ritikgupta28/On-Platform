c1feef0acc20c8c5762769a69ba8c81606ca18e6
Ritik Gupta <ritikgupta2003@gmail.com>
2020-08-30 23:08:36 +0530

    done

--- SSR_Website_Backend/routes/sendMessage.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const sendMessageController = require('../controllers/sendMessage');
-const router = express.Router();
-
-router.post('/sendMessage',sendMessageController.sendMessage);
-
-module.exports = router;
